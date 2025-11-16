65d7f2dcaf77dc355f99ce811685936c34aaac5e
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-16 16:27:17 -0400

    add AuthRepository interface

---
+++ app/src/main/java/edu/ucne/farmaciacruz/domain/repository/AuthRepository.kt
@@ -0,0 +1,19 @@
+package edu.ucne.farmaciacruz.domain.repository
+
+import edu.ucne.farmaciacruz.domain.model.Resource
+import edu.ucne.farmaciacruz.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepository {
+    suspend fun login(email: String, password: String): Flow<Resource<User>>
+    suspend fun logout()
+    fun isLoggedIn(): Flow<Boolean>
+    fun getUserData(): Flow<User?>
+    suspend fun register(
+        email: String,
+        password: String,
+        nombre: String,
+        apellido: String,
+        telefono: String?
+    ): Flow<Resource<User>>
+}
