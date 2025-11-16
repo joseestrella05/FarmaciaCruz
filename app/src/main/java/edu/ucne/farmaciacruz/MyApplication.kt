798cb25a4bab102d98fa55689b3ca69dbd39d4bb
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-16 15:58:56 -0400

    add hilt implementation and internet permission

---
+++ app/src/main/java/edu/ucne/farmaciacruz/MyApplication.kt
@@ -0,0 +1,12 @@
+package edu.ucne.farmaciacruz
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
