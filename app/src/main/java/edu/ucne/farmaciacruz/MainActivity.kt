0d2be5d0c2d7ac29d33b352e03455acbb970648e
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-16 23:42:17 -0400

    MainActivity.kt

--- app/src/main/java/edu/ucne/farmaciacruz/MainActivity.kt
+++ app/src/main/java/edu/ucne/farmaciacruz/MainActivity.kt
@@ -11,37 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.farmaciacruz.presentation.navigation.AppNavigation
 import edu.ucne.farmaciacruz.ui.theme.FarmaciaCruzTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FarmaciaCruzTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            FarmaciaCruzTheme() {
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FarmaciaCruzTheme {
-        Greeting("Android")
     }
 }
