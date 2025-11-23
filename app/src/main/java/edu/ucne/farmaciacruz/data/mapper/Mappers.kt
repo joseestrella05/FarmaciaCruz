8c32e7f3e508e74cb1ae54c8163ea5295f85a75e
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-23 01:45:29 -0400

    update Mappers

--- app/src/main/java/edu/ucne/farmaciacruz/data/mapper/Mappers.kt
+++ app/src/main/java/edu/ucne/farmaciacruz/data/mapper/Mappers.kt
@@ -1,8 +1,10 @@
 package edu.ucne.farmaciacruz.data.mapper
 
+import edu.ucne.farmaciacruz.data.local.entity.CarritoEntity
 import edu.ucne.farmaciacruz.data.remote.dto.AuthResponseDto
 import edu.ucne.farmaciacruz.data.remote.dto.ProductoDto
 import edu.ucne.farmaciacruz.data.remote.dto.UsuarioDto
+import edu.ucne.farmaciacruz.domain.model.CarritoItem
 import edu.ucne.farmaciacruz.domain.model.Producto
 import edu.ucne.farmaciacruz.domain.model.User
 
@@ -44,11 +46,41 @@ fun Producto.toDto(): ProductoDto {
     )
 }
 
-
 fun List<ProductoDto>.toDomain(): List<Producto> {
     return this.map { it.toDomain() }
 }
 
 fun List<Producto>.toDto(): List<ProductoDto> {
     return this.map { it.toDto() }
+}
+
+fun CarritoEntity.toDomain(): CarritoItem {
+    return CarritoItem(
+        producto = Producto(
+            id = this.productoId,          // ✅ Producto usa "id"
+            nombre = this.nombre,
+            categoria = this.categoria,
+            descripcion = this.descripcion,
+            precio = this.precio,
+            imagenUrl = this.imagenUrl
+        ),
+        cantidad = this.cantidad
+    )
+}
+
+fun Producto.toCarritoEntity(
+    usuarioId: Int,
+    cantidad: Int = 1
+): CarritoEntity {
+    return CarritoEntity(
+        id = 0,
+        usuarioId = usuarioId,
+        productoId = this.id,
+        cantidad = cantidad,
+        nombre = this.nombre,
+        categoria = this.categoria,
+        descripcion = this.descripcion,
+        precio = this.precio,
+        imagenUrl = this.imagenUrl
+    )
 }
