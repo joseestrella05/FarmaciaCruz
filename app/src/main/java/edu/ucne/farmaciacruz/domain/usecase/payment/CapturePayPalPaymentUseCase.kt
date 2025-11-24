b432966a09d3caaa68448f2a1bea56e779dd77c6
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-11-24 13:18:29 -0400

    Added CapturePayPalPaymentUseCase

---
+++ app/src/main/java/edu/ucne/farmaciacruz/domain/usecase/payment/CapturePayPalPaymentUseCase.kt
@@ -0,0 +1,18 @@
+package edu.ucne.farmaciacruz.domain.usecase.payment
+
+import edu.ucne.farmaciacruz.domain.model.PaymentResult
+import edu.ucne.farmaciacruz.domain.model.Resource
+import edu.ucne.farmaciacruz.domain.repository.PaymentRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CapturePayPalPaymentUseCase @Inject constructor(
+    private val repository: PaymentRepository
+) {
+    suspend operator fun invoke(
+        paypalOrderId: String,
+        localOrderId: String
+    ): Flow<Resource<PaymentResult>> {
+        return repository.capturePayPalPayment(paypalOrderId, localOrderId)
+    }
+}
